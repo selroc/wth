aa3f0fe186d86dd1602ad794bfa97f83f41de68f
dylngg <dylan@gardnermedia.com>
2018-08-15 22:18:04 -0600

    Make -l output prettier, misc bugfixes

--- wth.sh
+++ wth.sh
@@ -4,20 +4,27 @@
 # A dumb program that lets you record what you're doing by piping info into 
 # stdin. Records can be later run/viewed to see/do what you were working on. 
 # First line must start with a "#" and typically contain a brief description.
-# Each individual input is put in it's own script in ~/wth. Stdin is not 
-# sanitized. This is intended such that the recorded bash commands can be run to
-# restore windows and setup your previous work env. Not secure (duh.), use with 
-# extreme caution. 
+# Each individual input is put in it's own script in a specified location. 
+# Stdin is not  sanitized. This is intended such that the recorded bash 
+# commands can be run  to restore windows and setup your previous work env.
+# Not secure (duh.), use with extreme caution. 
 
-
+WTH_LOCATION=~/wth
 RECORD_NAME="record-`date +%FT%T`"
+NAME="untitled"
 PROCESS=0
 
+# check for help
 if [ "$1" == "--help" ]
 then
   echo "usage: wth.sh [-l | -r | -n]"
   echo ""
-  echo "Records commands and comments into a bash file that can be retrieved to restore what you were working on. Unless -l or -r args are provided, stdin is processed as a record into a file in ~/wth. Stdin is not sanitized. Requires sudo privilages."
+  cat <<EOF
+Records commands and comments into a bash file that can be retrieved to 
+restore what you were working on. Unless -l or -r args are provided, stdin is
+processed as a record into a file in ~/wth. Stdin is not sanitized.
+EOF
+  
   echo ""
   echo "optional arguments:"
   echo "    -n, --name            name of the record"
@@ -25,17 +32,34 @@ then
   echo "    -r, --random          A random record is selected and executed"
   exit 0
 
-elif [ $# -gt 1 ] 
+# if args
+elif [ $# -ge 1 ] 
 then
   if [ "$1" == '-l' ] || [ "$1" == '--list' ]
   then
-    # list the records
-    ls -Gt ~/wth/record*.sh
-    PROCESS=1
+    # loop through the records
+    for f in $WTH_LOCATION/record*.sh
+    do
+      RECORD_FN=`basename "$f"`
+      FN=`echo "$RECORD_FN" | awk -F '-' '{ print $5 }' | sed 's/.sh//'`
+      RECORD_DATE=`echo "$RECORD_FN" | sed 's/record-//' | sed "s/-$FN.sh//g" | sed 's/T/ at /' | sed 's;-;/;g'`
+
+      # print filename: (record-filename)
+      printf "%-23s %-25s %s\n" "($RECORD_DATE)" "$FN:" "($RECORD_FN)"
+
+      # print first 2 lines of record
+      echo "--------------------------------------------"
+      cat $f | head -2
+      echo ""
+    done
+    
+    PROCESS=1  # indicate we shouldn't process stdin
+  
+  # if looking to run a random record
   elif [ "$1" == '-r' ] || [ "$1" == '--random' ]
   then
     # list the records, randomize it, get the first record
-    file=`ls -Gt ~/wth/record*.sh | sort -R | tail -1`
+    file=`ls -Gt $WTH_LOCATION/record*.sh | sort -R | tail -1`
     while read -r line
     do 
       if [[ ${line:0:1} == "#" ]]
@@ -48,8 +72,9 @@ then
       fi 
     done < $file
 
-    PROCESS=1  # indicate that we are not processing input
- 
+    PROCESS=1  # indicate we shouldn't process stdin
+
+  # if specifying name for record
   elif [ "$1" == '-n' ] || [ "$1" == '--name' ]
   then
     # if no second arg
@@ -66,19 +91,17 @@ then
     CLEAN=${CLEAN// /_}
     # clean out anything that's not alphanumeric or an underscore
     CLEAN=${CLEAN//[^a-zA-Z0-9_]/}
-    # lowercase with TR
-    RECORD_NAME="$RECORD_NAME-`echo $CLEAN | tr A-Z a-z`"
+    # convert to lowercase
+    NAME="`echo $CLEAN | tr A-Z a-z`"
   else
     echo "Invalid Arguments. See --help"
   fi
 fi
+
+# if we need to process stdin
 if [ $PROCESS -eq 0 ]
 then
-  while read stdin
-  do
-    # write the input out to the record
-    echo $stdin > ~/wth/$RECORD_NAME.sh
-  done
-  echo "Added record to file: ~/wth/$RECORD_NAME.sh"
+  cat >> $WTH_LOCATION/$RECORD_NAME-$NAME.sh
+  echo "Added record to file: $WTH_LOCATION/$RECORD_NAME-$NAME.sh"
   exit 0
 fi
